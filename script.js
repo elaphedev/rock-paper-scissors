b3e919c9cb400b1da130cb1cae3bef2a5c145609
Elaph <185706300+elaph-2@users.noreply.github.com>
2024-11-28 11:00:28 -0300

    Add playGame() and test it.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 // Makes the computer's choice.
 // Randomly chooses between rock, paper or scissors.
 function getComputerChoice() {
@@ -36,33 +33,56 @@ function getHumanChoice() {
   }
 }
 
-// Plays a sinlge round of rock, paper, scissors.
-// Determines who is the winner and increments their respective score.
-function playRound(humanChoice, computerChoice) {
-  alert(`Your choice: ${humanChoice}\nThe computer's choice: ${computerChoice}`);
+// Plays a game of rock paper scissors.
+// Determines a winner after 5 rounds.
+function playGame() {
+  alert("Welcome to rock, paper, scissors! A game where you play five rounds of rock paper scissors against the computer!");
 
-  // Checks if the human wins
-  if (humanChoice == "rock" && computerChoice == "scissors" ||
-      humanChoice == "paper" && computerChoice == "rock" ||
-      humanChoice == "scissors" && computerChoice == "paper") {
-    humanScore += 1;
-    alert("You win!");
-    return;
+  let humanScore = 0;
+  let computerScore = 0;
+
+  // Plays a sinlge round of rock, paper, scissors.
+  // Determines who is the winner and increments their respective score.
+  function playRound(humanChoice, computerChoice) {
+    alert(`Your choice: ${humanChoice}\nThe computer's choice: ${computerChoice}`);
+
+    // Checks if the human wins
+    if (humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper") {
+      humanScore += 1;
+      alert("You win!");
+      return;
+    }
+
+    // Checks if the computer wins
+    if (computerChoice == "rock" && humanChoice == "scissors" ||
+        computerChoice == "paper" && humanChoice == "rock" ||
+        computerChoice == "scissors" && humanChoice == "paper") {
+      computerScore += 1;
+      alert("You lose! The computer wins!");
+      return;
+    }
+
+    // If neither of those previous conditions are met
+    // this means it was a draw.
+    alert("Draw! Better luck next time!");
   }
 
-  // Checks if the computer wins
-  if (computerChoice == "rock" && humanChoice == "scissors" ||
-      computerChoice == "paper" && humanChoice == "rock" ||
-      computerChoice == "scissors" && humanChoice == "paper") {
-    computerScore += 1;
-    alert("You lose! The computer wins!");
-    return;
+  for (let i = 0; i < 5; i++) {
+    alert(`You: ${humanScore}\nComputer: ${computerScore}`);
+    playRound(getHumanChoice(), getComputerChoice());
   }
 
-  // If neither of those previous conditions are met
-  // this means it was a draw.
-  alert("Draw! Better luck next time!");
-  return;
+  if (humanScore > computerScore) {
+    alert(`Congratulations! You won the game!\n\nYou: ${humanScore}\nComputer: ${computerScore}`);
+  }
+  else if (computerScore > humanScore) {
+    alert(`Awww, you lost the game! Better luck next time!\n\nYou: ${humanScore}\nComputer: ${computerScore}`);
+  }
+  else {
+    alert(`You tied with the computer!\n\nYou: ${humanScore}\nComputer: ${computerScore}`);
+  }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
