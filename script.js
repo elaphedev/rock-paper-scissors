2b09ea97b9f3d1a97fbff0c6fa25f4b8821839ed
Elaph <185706300+elaph-2@users.noreply.github.com>
2024-11-28 09:55:52 -0300

    Write and test getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello, world! 2!");
+function getComputerChoice() {
+  const MAX = 3; // Generates, at the maximum, 3 different random values (between 0-2).
+  const randomValue = Math.floor(Math.random() * MAX);
+
+  switch (randomValue) {
+    case 0: {
+      return "rock";
+    }
+    case 1: {
+      return "paper";
+    }
+    case 2: {
+      return "scissors";
+    }
+  }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
