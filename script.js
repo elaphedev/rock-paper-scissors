ea1963204fd0b35c67d1220a0b568aa6478ec746
Elaph <185706300+elaph-2@users.noreply.github.com>
2024-11-28 10:10:51 -0300

    Write and test getHumanChoice()

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Makes the computer's choice.
+// Randomly chooses between rock, paper or scissors.
 function getComputerChoice() {
   const MAX = 3; // Generates, at the maximum, 3 different random values (between 0-2).
   const randomValue = Math.floor(Math.random() * MAX);
@@ -15,11 +17,20 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+// Prompts user for a choice.
+// Will not let user choose an invalid option.
+function getHumanChoice() {
+  while (true) {
+    let humanChoice = prompt("Rock, paper or scissors?");
+    humanChoice = humanChoice.toLocaleLowerCase();
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+      return humanChoice;
+    }
+    else {
+      alert("Invalid option! Please try again.");
+      continue;
+    }
+  }
+}
+
+console.log(getHumanChoice());
